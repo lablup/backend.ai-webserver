291c79d49485784bb0709354061254115cc91633
Jeongkyu Shin <inureyes@gmail.com>
2020-03-03 18:18:16 +0900

    Modify multi-line statements (backend.ai#169)

--- src/ai/backend/console/server.py
+++ src/ai/backend/console/server.py
@@ -89,7 +89,7 @@ async def console_handler(request: web.Request) -> web.StreamResponse:
     request_path = request.match_info['path']
     file_path = (static_path / request_path).resolve()
     config = request.app['config']
-    if 'force-endpoint-protocol' in config['service'] and 
+    if 'force-endpoint-protocol' in config['service'] and \
         config['service']['force-endpoint-protocol'] in ['http', 'https']:
         scheme = config['service']['force-endpoint-protocol']
     else:
