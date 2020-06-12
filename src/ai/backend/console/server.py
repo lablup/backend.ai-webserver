2b180bc17d60064347832e75f678a4fbd20b90a0
Joongi Kim <joongi@lablup.com>
2020-06-12 12:04:57 +0900

    fix: The default must be an empty string instead of "None" when rendered

--- src/ai/backend/console/server.py
+++ src/ai/backend/console/server.py
@@ -132,7 +132,7 @@ async def console_handler(request: web.Request) -> web.StreamResponse:
                 'true' if config['service'].get('allow_anonymous_change_password') else 'false',
             'allow_project_resource_monitor':
                 'true' if config['service']['allow_project_resource_monitor'] else 'false',
-            'menu_blocklist': config['ui'].get('menu_blocklist'),
+            'menu_blocklist': config['ui'].get('menu_blocklist', ''),
             'license_edition': license_edition if license_edition is not None else 'Open Source',
             'license_valid_since': license_valid_since if license_valid_since is not None else '',
             'license_valid_until': license_valid_until if license_valid_until is not None else '',
